8a06cd1138b30849154cf4a35670c4a5b9a69f5c
evren korkmaz <evrennkorkmaz@gmail.com>
2019-09-06 11:31:26 +0300

    Update raw_server.py

--- raw_server.py
+++ raw_server.py
@@ -1,8 +1,15 @@
+# Author : Evren Korkmaz
+# This python project create a socket on localhost port 3000
+# and take a package from client side.
 import socket
 import sys
 import os
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW,1) #create a raw socket
-ip = ("127.0.0.1",3000) # local raw socket adress and port
+
+# Create a socket object.
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW,1) 
+# define a socket port
+ip = ("127.0.0.1",3000)
+# bind and wait package
 s.bind(ip)
 
 print("Connecting..")
